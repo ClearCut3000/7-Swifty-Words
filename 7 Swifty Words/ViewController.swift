4f0317f1cac3b754a98e976e14ed1f9f5b5c3f95
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2021-12-19 12:42:53 +0300

    Initial Commit

---
+++ 7 Swifty Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  7 Swifty Words
+//
+//  Created by Николай Никитин on 19.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
